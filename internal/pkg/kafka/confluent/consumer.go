274699df68c7d187dd184eb2eecf187471a90c29
Alif Septian Nurdianto <alifsn@Alifs-MacBook-Pro.local>
2024-03-20 09:33:54 +0700

    fix: change topic name

--- internal/pkg/kafka/confluent/consumer.go
+++ internal/pkg/kafka/confluent/consumer.go
@@ -51,10 +51,10 @@ func (c *consumer) Subscribe(topics ...string) {
 				continue
 			}
 			switch topics[0] {
-			case "ticket-concert-send-otp-user-registration":
+			case "asm-concert-send-otp-user-registration":
 				go c.handler.SendEmailOtpRegister(msg, topics[0])
 				c.consumer.CommitMessage(msg)
-			case "ticket-concert-send-email-pdf":
+			case "asm-concert-send-email-pdf":
 				go c.handler.SendEmailTicket(msg, topics[0])
 				c.consumer.CommitMessage(msg)
 			default:
